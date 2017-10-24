25e08b0bed06f22680f39e45c0b73b7e5522f798
Fantasticlese <joshuathorup@gmail.com>
2017-10-24 14:25:59 -0600

    JThorup tool initial commit

---
+++ lib/jthorup_view_tool.rb
@@ -0,0 +1,6 @@
+require "jthorup_view_tool/version"
+require "jthorup_view_tool/renderer"
+
+module JthorupViewTool
+  # Your code goes here...
+end
---
+++ lib/jthorup_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JthorupViewTool
+  VERSION = "0.1.0"
+end
